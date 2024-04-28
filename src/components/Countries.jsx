5d50dab855ce5fc1becbb11ab84b96fc39544acc
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-04-29 04:09:33 +0600

    country component added

---
+++ src/components/Countries.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import ShowCountry from "./ShowCountry";
+
+const Countries = () => {
+    // const countries = useLoaderData()
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/country")
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {
+                countries.map((country) => <ShowCountry 
+                key={country._id} 
+                country={country}
+                >
+                </ShowCountry>
+            
+            )
+            }
+            </div>
+    );
+};
+
+export default Countries;
